ad0be65277a086a4f93b70e9dae18b2388ecf71a
xiaoHan <qiaoming@zcool.com.cn>
2018-11-17 19:17:35 +0800

    baidu verify

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <title>dodo-小寒的博客-用心创作</title>
+          <meta name="baidu-site-verification" content="hd4JTXEMxD" />
           <meta name="keywords" content="" />
           <meta name="description" content="超级有趣的网站，学点技术，学点思想，学点设计" />
           <link rel="shortcut icon" href="/static/favicon.ico" />
