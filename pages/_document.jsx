8dd095f8077e6a373b28a4ded545b94a87708b46
xiaoHan <qiaoming@zcool.com.cn>
2018-11-20 16:04:43 +0800

    f

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -16,7 +16,6 @@ export default class MyDocument extends Document {
           <meta name="keywords" content="前端,设计,技术,文章,个人博客,什么都写,边听歌边看博客" />
           <meta name="description" content="超级有趣的网站，学点技术，学点思想，学点设计" />
           <link rel="shortcut icon" href="/static/favicon.ico" />
-          <link rel="stylesheet" href="/_next/static/css/styles.chunk.css" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/agate.min.css"></link>
           <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" name="viewport" />
         </Head>
