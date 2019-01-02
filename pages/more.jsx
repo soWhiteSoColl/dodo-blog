b79f02e26a95341161340b6b75ac66f64692b35d
xiaoHan <qiaoming@zcool.com.cn>
2019-01-02 19:13:35 +0800

    f

--- pages/more.jsx
+++ pages/more.jsx
@@ -9,37 +9,43 @@ export default class More extends React.Component {
       name: 'zeus-ui 组件库',
       link: 'https://zeus-ui.com',
       codeLink: 'https://github.com/zcued/zeus-doc',
-      cover: '/static/works/zeus-ui.png'
+      cover: '/static/works/zeus-ui.png',
+      desc: '开发站酷海洛集成的组件库, 访问www.hellorf.com'
     },
     {
       name: 'dodoui 组件库',
       link: 'https://ui.dodoblog.cn',
       codeLink: 'https://github.com/soWhiteSoColl/dodoui',
-      cover: '/static/works/dodo-ui.png'
+      cover: '/static/works/dodo-ui.png',
+      desc: '交互体验很好也蛮有意思的一些组件'
     },
     {
       name: '扫雷游戏',
       link: 'https://sowhitesocoll.github.io/clear-mine/mine/mine.html',
       codeLink: 'https://github.com/soWhiteSoColl/clear-mine',
-      cover: '/static/works/clear-mine.png'
+      cover: '/static/works/clear-mine.png',
+      desc: '大学写的小游戏'
     },
     {
       name: 'canvas粒子动画',
       link: 'https://sowhitesocoll.github.io/text-proticle/drawtext.html',
       codeLink: 'https://github.com/soWhiteSoColl/text-proticle',
-      cover: '/static/works/proticle.png'
+      cover: '/static/works/proticle.png',
+      desc: '自己动画开发技术的一个很满意的作品'
     },
     {
       name: '图片动画展示',
       link: 'https://sowhitesocoll.github.io/imgShow/imageCss.html',
       codeLink: 'https://github.com/soWhiteSoColl/imgShow',
-      cover: '/static/works/img-show.png'
+      cover: '/static/works/img-show.png',
+      desc: '强大的css3 transform的一个运用'
     },
     {
       name: '鼠标追踪动画',
       link: 'https://sowhitesocoll.github.io/mousemoveAnimation/',
       codeLink: 'https://github.com/soWhiteSoColl/mousemoveAnimation',
-      cover: '/static/works/mouse.png'
+      cover: '/static/works/mouse.png',
+      desc: 'canvas 3d交互效果，自己很喜欢的一个小项目'
     }
   ]
 
@@ -64,8 +70,10 @@ export default class More extends React.Component {
                       <a target="_blank" href={item.codeLink}><Button>项目地址</Button></a>
                     </div>
                   </div>
-                  <div className="contact-project-name">
-                    {item.name}
+                  <div className="contact-project-info">
+                    <div className="contact-project-name">
+                      {item.name}
+                    </div>
                   </div>
                 </div>
               </div>
