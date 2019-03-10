b9c32fb55765668246bdc776cc8f1c5391ac7fa6
qm <1256790127@qq.com>
2019-03-10 16:23:02 +0800

    fix style

--- components/widgets/Editor.jsx
+++ components/widgets/Editor.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import Head from 'next/head'
 import BraftEditor from 'braft-editor'
-import 'braft-editor/dist/index.css'
-
+import editorcss from 'braft-editor/dist/index.css'
 
 const colors = [
-  '#000000', '#333333', '#666666', '#999999', '#cccccc', '#ffffff',
-  '#61a951', '#16a085', '#07a9fe', '#003ba5', '#8e44ad', '#f32784',
-  '#c0392b', '#d35400', '#f39c12', '#fdda00', '#7f8c8d', '#2c3e50'
+  '#000000',
+  '#333333',
+  '#666666',
+  '#999999',
+  '#cccccc',
+  '#ffffff',
+  '#61a951',
+  '#16a085',
+  '#07a9fe',
+  '#003ba5',
+  '#8e44ad',
+  '#f32784',
+  '#c0392b',
+  '#d35400',
+  '#f39c12',
+  '#fdda00',
+  '#7f8c8d',
+  '#2c3e50'
 ]
 
-const controls = [
-  'bold', 'italic', 'underline', 'text-color', 'remove-styles',
-  'emoji', 'link'
-]
+const controls = ['bold', 'italic', 'underline', 'text-color', 'remove-styles', 'emoji', 'link']
 
 export default class Editor extends React.Component {
   state = {
@@ -28,14 +40,14 @@ export default class Editor extends React.Component {
 
   render() {
     return (
-      <div className={'editor-wrapper'}>
-        <BraftEditor
-          controls={controls}
-          colors={colors}
-          {...this.props}
-          value={this.state.value}
-        />
-      </div>
+      <>
+        <Head>
+          <style>{editorcss}</style>
+        </Head>
+        <div className={'editor-wrapper'}>
+          <BraftEditor controls={controls} colors={colors} {...this.props} value={this.state.value} />
+        </div>
+      </>
     )
   }
 }
