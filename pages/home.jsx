4795f7e41579ea694d04944e41d2f519419381d5
qm <1256790127@qq.com>
2019-03-13 21:27:20 +0800

    f

--- pages/home.jsx
+++ pages/home.jsx
@@ -3,14 +3,75 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Router from 'next/router'
 
+function writeOneByOne(text, dom, container, opt = {}) {
+  const el = document.createElement(dom)
+  const bar = document.createElement('span')
+  bar.className = 'write-vertical-bar'
+  el.appendChild(bar)
+  container.appendChild(el)
+
+  let timer = null,
+    currentIndex = 0,
+    interval = opt.interval || 100,
+    sleep = opt.sleep || 0
+
+  return new Promise(resolve => {
+    function next() {
+      const textNode = document.createTextNode(text[currentIndex])
+      el.insertBefore(textNode, bar)
+
+      currentIndex++
+      if (currentIndex < text.length) {
+        timer = setTimeout(() => {
+          next()
+        }, interval)
+      } else {
+        timer = setTimeout(() => {
+          el.removeChild(bar)
+          clearTimeout(timer)
+          resolve()
+        }, sleep)
+      }
+    }
+
+    next()
+  })
+}
+
 export default class Home extends React.Component {
-  canvasBg = React.createRef()
+  $container = React.createRef()
 
   static getInitialProps() {
     return { footer: false, header: false }
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    const container = this.$container.current
+
+    if (!this.props.configStore.isWriteEnd) {
+      writeOneByOne('嗨哟。', 'h2', container, { interval: 150, sleep: 300 })
+        .then(() => writeOneByOne('我是小寒，', 'h4', container, { interval: 150, sleep: 300 }))
+        .then(() => writeOneByOne('一名前端开发工程师，也是这个网站的创建者和开发者。', 'p', container, { sleep: 400 }))
+        .then(() =>
+          writeOneByOne('在这个网站里你可以愉快的阅读我写的文章，也能听听各种音乐。', 'p', container, { sleep: 400 })
+        )
+        .then(() =>
+          writeOneByOne(
+            '相信大多数的来到这个网站的人都是和互联网技术有关的人，如果想要获取源码或者和我有更加亲密的认识，可以通过邮箱1256790127@qq.com联系我，也可以加我的微信q1256790127。',
+            'p',
+            container,
+            { sleep: 400 }
+          )
+        )
+        .then(() => writeOneByOne('非常欢迎各种不速之客来对我的网站进行评价和指点。', 'p', container))
+
+      const onRouterChange = () => {
+        this.props.configStore.isWriteEnd = true
+        Router.events.off('routeChangeStart', onRouterChange)
+      }
+      Router.events.on('routeChangeStart', onRouterChange)
+    }
+  }
 
   handleLogOut = () => {
     this.props.userStore.logOut()
@@ -18,6 +79,8 @@ export default class Home extends React.Component {
 
   render() {
     const { info } = this.props.userStore
+    const { isWriteEnd } = this.props.configStore
+
     return (
       <React.Fragment>
         <Head>
@@ -27,30 +90,20 @@ export default class Home extends React.Component {
         </Head>
         <div className="home-page">
           <div className="home-container">
-            <div className="home-desc">
-              <h2>嗨哟。</h2>
-              <h4>
-                我是<em>小寒</em>，
-              </h4>
-              <p>一名前端开发工程师，也是这个网站的创建者和开发者。</p>
-              <p>
-                在这个网站里你可以愉快的
-                <Link href="/">
-                  <a>阅读我写的文章</a>
-                </Link>
-                ，也能
-                <Link href="/music/list">
-                  <a>听听各种音乐</a>
-                </Link>
-                。{' '}
-              </p>
-              <p>
-                我相信来到这个网站的大多数人都是开发或者和互联网技术有关的人，如果想要获取源码或者和我有更加亲密的认识，可以通过邮箱
-                <em>1256790127@qq.com</em>联系我，也可以加我的微信<em>q1256790127</em>。
-              </p>
-              <p>非常欢迎各种不速之客来对我的网站进行评价和指点。</p>
+            <div className="home-desc" ref={this.$container}>
+              {isWriteEnd && (
+                <>
+                  <h2>嗨哟。</h2>
+                  <h4>我是小寒，</h4>
+                  <p>一名前端开发工程师，也是这个网站的创建者和开发者。</p>
+                  <p>在这个网站里你可以愉快的阅读我写的文章，也能听听各种音乐。</p>
+                  <p>
+                    我相信来到这个网站的大多数人都是开发或者和互联网技术有关的人，如果想要获取源码或者和我有更加亲密的认识，可以通过邮箱1256790127@qq.com联系我，也可以加我的微信q1256790127。
+                  </p>
+                  <p>非常欢迎各种不速之客来对我的网站进行评价和指点。</p>
+                </>
+              )}
             </div>
-
             <div className="home-links">
               <Link href="/">
                 <a className="home-link">博客</a>
--- stores/index.js
+++ stores/index.js
@@ -1,9 +1,11 @@
 import BlogStore from './blog'
 import MusicStore from './music'
 import UserStore from './user'
+import ConfigStore from './config'
 
 export default {
   blogStore: new BlogStore(),
   musicStore: new MusicStore(),
-  userStore: new UserStore()
+  userStore: new UserStore(),
+  configStore: new ConfigStore()
 }
