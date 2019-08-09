0f8b49e5c5af0a15f3b452a584962f176dd5b307
qiaoming <1256790127@qq.com>
2019-08-09 11:08:23 +0800

    fix:

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -26,6 +26,20 @@ export default class MyDocument extends Document {
         <body className="custom_class">
           <Main />
           <NextScript />
+          <script>
+            {`window._pt_lt = new Date().getTime();
+              window._pt_sp_2 = [];
+              _pt_sp_2.push("setAccount,26c1d230");
+              var _protocol =(("https:" == document.location.protocol) ? " https://" : " http://");
+              (function() {
+                var atag = document.createElement("script");
+                atag.type = "text/javascript";
+                atag.async = true;
+                atag.src = _protocol + "js.ptengine.cn/26c1d230.js";
+                var s = document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(atag, s);
+              })();`}
+          </script>
         </body>
       </html>
     )
