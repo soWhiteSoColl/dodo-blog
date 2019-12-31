5e749a18634ecc4f6e5921787c6ca4aff083775f
unknown <ming.qiao@zhangmen.com>
2019-12-31 17:28:54 +0800

    init: 项目初始化

---
+++ pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>小寒的博客-用心创作</title>
+          <meta name="baidu-site-verification" content="hd4JTXEMxD" />
+          <meta
+            name="keywords"
+            content="前端,设计,技术,文章,个人博客,什么都写,边听歌边看博客"
+          />
+          <meta
+            name="description"
+            content="小寒的博客，用来分享自己的知识，欢迎大家关注"
+          />
+          <meta
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;"
+            name="viewport"
+          />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <link rel="manifest" href="/static/lib/manifest.json"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
---
+++ pages/index/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './index.scss'
+
+export default class App extends React.Component {
+  render() {
+    return <div>app</div>
+  }
+}
